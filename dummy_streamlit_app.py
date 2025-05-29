7f064f596dc5d62cbd5a30a405a9b7849156f52f
burkardj91 <johannes.burkard@outlook.com>
2025-05-29 12:01:41 +0200

    Delete dummy_streamlit_app.py

--- dummy_streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Packaged Streamlit App")
-st.title("🎯 Standalone Streamlit App")
-st.success("This app was packaged into an .exe and launched directly!")
-
